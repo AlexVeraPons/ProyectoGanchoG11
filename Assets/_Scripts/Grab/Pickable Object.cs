74a79751e873bbe42c7877dfac051d5457a5f73c
Ruben Lopez <rlopezg@edu.tecnocampus.cat>
2023-04-14 21:28:59 +0200

    Wip

---
+++ Assets/_Scripts/Grab/Pickable Object.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour, IPickable
+{
+    public void OnPicked()
+    {
+
+    }
+}
