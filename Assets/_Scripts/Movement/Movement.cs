fbf83eaae3f702e3be21647cb759629012338616
Alexandro Vera <Alexandroverapons@gmail.com>
2023-04-14 18:35:40 +0200

    Started movement

---
+++ Assets/_Scripts/Movement/Movement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float _speed = 5f;
+    private float _acceleration = 0.1f;
+    private Rigidbody2D _rigidbody2D;
+}
