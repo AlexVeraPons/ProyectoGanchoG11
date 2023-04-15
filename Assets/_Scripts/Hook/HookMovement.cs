2991796e58880c6547c5f61dfed4093db45b1e87
Eduard Sales <EvilHat2@gmail.com>
2023-04-15 22:49:08 +0200

    trials de hook
    passame la hooka

--- Assets/_Scripts/Hook/HookMovement.cs
+++ Assets/_Scripts/Hook/HookMovement.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 
 public class HookMovement : MonoBehaviour
 {
-    public static Action OnHookEntersPlayer;
+    public static Action<bool> OnHookEntersPlayer;
+    public DistanceJoint2D _Distancejoint;
     private Rigidbody2D _rigidBody2D;
     public bool MovingRight = false;
     private float _speed;
     private Vector2 _direction;
+    private Vector2 _direction2;
     private int _timesItColidesWithParent = 2;
+    public LineRenderer _lineRenderer;
+    //---
+    private float timer = 2;
+
     void OnEnable()
     {
         ThowHook.hookIsMovingRight += ChangeHookDirection;
@@ -29,6 +35,7 @@ public class HookMovement : MonoBehaviour
     {
         _rigidBody2D = GetComponent<Rigidbody2D>();
         _speed = 100;
+        _lineRenderer.enabled = true;
     }
 
     void FixedUpdate()
@@ -37,10 +44,20 @@ public class HookMovement : MonoBehaviour
         {
             _rigidBody2D.velocity = Vector2.right * _speed * Time.deltaTime;
         }
-        else
+        else 
         {
-            _rigidBody2D.velocity = Vector2.left * _speed * Time.deltaTime;
+            var padrePos = this.transform.parent.transform.position;
+            var directionDeVuelta = padrePos - this.transform.position;
+            _rigidBody2D.velocity = directionDeVuelta.normalized * _speed * Time.deltaTime;
+            
         }
+    }
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        _lineRenderer.SetPosition(0, this.transform.parent.transform.position); //pos final de la linea, mouse
+        _lineRenderer.SetPosition(1, transform.position); //pos inicial de la linea, pj
+        
     }
     void ChangeHookDirection(bool movingRight)
     {
@@ -55,6 +72,8 @@ public class HookMovement : MonoBehaviour
         }
         if (_timesItColidesWithParent <= 0)
         {
+            _lineRenderer.enabled = false;
+            OnHookEntersPlayer?.Invoke(true);
             Destroy(gameObject);
 
         }
--- Assets/_Scripts/Hook/ThowHook.cs
+++ Assets/_Scripts/Hook/ThowHook.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 
 public class ThowHook : MonoBehaviour
 {
-    public GameObject BulletPrefab;
+    public GameObject HookPrefab;
     public static Action<bool> hookIsMovingRight;
+    private bool _canThrow;
 
     public Vector2 InputtedDirection { get; private set; }
 
+    void OnEnable()
+    {
+        HookMovement.OnHookEntersPlayer += enableThrow;
+    }
+    void OnDisable()
+    {
+        HookMovement.OnHookEntersPlayer -= enableThrow;
+
+    }
     void Start()
     {
-
+        _canThrow = true;
     }
 
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            hookIsMovingRight?.Invoke(true);
-            Throw();
+            if (_canThrow)
+            {
+                hookIsMovingRight?.Invoke(true);
+                Throw();
+            }
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
@@ -30,9 +44,18 @@ public class ThowHook : MonoBehaviour
     }
     void Throw()
     {
-        var hook = Instantiate(BulletPrefab, transform.position, Quaternion.identity);
+
+        _canThrow = false;
+        var hook = Instantiate(HookPrefab, transform.position, Quaternion.identity);
         var hookMovement = hook.GetComponent<HookMovement>();
         hookMovement.SetDirection(InputtedDirection);
         hook.transform.SetParent(this.transform);
+
+    }
+    void enableThrow(bool canHook)
+    {
+        _canThrow = canHook;
+        hookIsMovingRight?.Invoke(true);
+
     }
 }
