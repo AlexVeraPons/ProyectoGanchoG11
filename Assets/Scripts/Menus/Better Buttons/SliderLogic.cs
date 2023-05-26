124d5098f5657f77573a7c51c9b4794e7ddae2c8
Eduard Sales <EvilHat2@gmail.com>
2023-05-26 17:59:41 +0200

    escButtons

--- Assets/Scripts/Menus/Better Buttons/SliderLogic.cs
+++ Assets/Scripts/Menus/Better Buttons/SliderLogic.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 using UnityEngine;
 using System;
-using TMPro;
+
 public class SliderLogic : MonoBehaviour
 {
     private MenusMap _myInput;
--- Assets/Scripts/Menus/PauseSystem.cs
+++ Assets/Scripts/Menus/PauseSystem.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.InputSystem;
+
 
 public class PauseSystem : MonoBehaviour
 {
