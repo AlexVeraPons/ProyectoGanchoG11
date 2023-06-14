821bf787fb4d7df90bb85d6f38fb7a97aa8d31a3
Eduard Sales <EvilHat2@gmail.com>
2023-06-14 17:31:25 +0200

    fondo now animado

--- Assets/Scripts/Background/ChangeBackgroundImage.cs
+++ Assets/Scripts/Background/ChangeBackgroundImage.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ChangeBackgroundImage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+    private float _speed = 10;
+
+    private void Awake()
     {
-        
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("space");
+            ChangeToBossBackground();
+        }
+    }
+    private void ChangeToBossBackground()
     {
-        
+        _animator.SetTrigger("start");
+        //_spriteRenderer.sprite = _bossBackground;
+        Debug.Log("bossBackground");
+    }
+    private void ChangeToOriginalBackground()
+    {
+        //_spriteRenderer.sprite = _background;
+        Debug.Log("OGbackground");
+
     }
 }
