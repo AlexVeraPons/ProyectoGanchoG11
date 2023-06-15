3e0ddffe35a8e3641d756e8162cddb9cd8ad13b6
Alexandro Vera <Alexandroverapons@gmail.com>
2023-06-15 22:23:17 +0200

    done

--- Assets/Scripts/Mouse/Curosr.cs
+++ Assets/Scripts/Mouse/Curosr.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class Curosr : MonoBehaviour
 {
     [SerializeField]
-    private Texture2D _cursorTexture;
+    private Texture2D _cursorPressedTexture;
+    private Texture2D _cursorDefaultTexture;
 
     private void Start()
     {
         Cursor.visible = false;
+        _cursorDefaultTexture = this.gameObject.GetComponent<SpriteRenderer>().sprite.texture;
     }
 
     private void Update()
     {
         // this position to mouse position
         this.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y ,0);
+
+        //if mouse is down change texture to pressed until it is released
+        if (Input.GetMouseButton(0))
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Sprite.Create(_cursorPressedTexture, new Rect(0, 0, _cursorPressedTexture.width, _cursorPressedTexture.height), new Vector2(0.5f, 0.5f));
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Sprite.Create(_cursorDefaultTexture, new Rect(0, 0, _cursorDefaultTexture.width, _cursorDefaultTexture.height), new Vector2(0.5f, 0.5f));
+        }
     }
 }
